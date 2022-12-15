eba2096fb48645c44c6d7166bef609e8ac64ac31
Valapvil26 <valentinaapontevillamil@gmail.com>
2022-12-14 21:46:56 -0500

    Actualización Proyecto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,142 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from "./data/ghibli/ghibli.js";
+import { filterDir, newPelis, oldPelis,} from "./data.js";
 
-console.log(example, data);
+const pelis = data.films;
+
+
+
+/**------------- FUNCIÓN PARA VISUALIZAR LA DATA ----------------**/
+
+function visualizarPelis(dataMovies) {
+
+    const peliculas = document.getElementById("tarjetas");
+    peliculas.innerHTML = "";
+    for (let i = 0; i < dataMovies.length; i++) {
+        peliculas.innerHTML += `
+    <div class = "portada">
+    <img src="${dataMovies[i].poster}" alt"">
+
+    <button class="boton_id" id="boton_id">+</button>
+    <section class="modal" id="modal"></section>
+
+
+    <div class = "textos_portada">
+        <h2>${dataMovies[i].title}</h2>
+        <p>${dataMovies[i].release_date}</p>
+        <h2>${dataMovies[i].director}</h2>
+        </div>
+    </div>`
+    }
+}
+window.onload = () => { visualizarPelis(data.films) };
+
+
+
+
+// /**------------- FUNCIÓN PARA VISUALIZAR EL SCORE Y EL TITULO----------------**/
+//   function visualizarScore(scoreFilms) {
+//       const scorePelis = document.getElementById("statistics");
+//       scorePelis.innerHTML = "";
+//       for (let i = 0; i < scoreFilms.length; i++) {
+//         scorePelis.innerHTML += `
+//         <div class = "textos_score">
+//         <h2>${scoreFilms[i].title}</h2>
+//         <h2>${scoreFilms[i].rt_score}</h2>
+//         </div>
+//     `
+//   }
+// }
+// window.onload = () => { visualizarScore(data.films) };
+
+
+// // const scoreFilms = document.getElementById("statistics");
+// //   scoreFilms.textContent = score (rt_score, "score");
+
+// /**------------- EVENTO CLICK PARA EL MENU STATISTICS ----------------**/
+// const puntaje = document.getElementById("statistics");
+//   puntaje.addEventListener("click", () => {
+//     const punScore = score (pelis)
+//       visualizarScore(punScore)
+// });
+
+
+
+
+/**------------- EVENTO CLICK PARA EL MENU HOME ----------------**/
+
+const home = document.getElementById("home")
+home.addEventListener("click", () => {
+    visualizarPelis(pelis)
+});
+
+
+
+/**------------- EVENTO CLICK PARA EL MENU DIRECTORES ----------------**/
+
+const directoresPel = document.querySelectorAll(".menu-dir");
+
+directoresPel.forEach((director) => {
+    director.addEventListener("click", () => {
+        const showDirector = filterDir(pelis, director.id)
+        visualizarPelis(showDirector);
+        // console.log(showDirector);
+    })
+});
+
+/**------------- EVENTO CLICK PARA ORDENAR PELICULAS DE LA MAS RECIENTE A LA MENOS RECIENTE ----------------**/
+
+const newestMovies = document.getElementById("newMovies")
+newestMovies.addEventListener("click", () => {
+    const showNewestMovies = newPelis(pelis)
+    visualizarPelis(showNewestMovies);
+});
+
+/**------------- EVENTO CLICK PARA ORDENAR PELICULAS DE LA MAS RECIENTE A LA MENOS RECIENTE ----------------**/
+
+const oldestMovies = document.getElementById("oldMovies")
+oldestMovies.addEventListener("click", () => {
+    const showOldestMovies = oldPelis(pelis)
+    visualizarPelis(showOldestMovies);
+});
+
+/**--------- FUNCIÓN PARA FLECHA UP */
+
+window.onscroll = function(){
+  if(document.documentElement.scrollTop > 100){
+    document.querySelector(".contenedor_boton_top")
+    .classList.add("show");
+  }else{
+    document.querySelector(".contenedor_boton_top")
+    .classList.remove("show");
+  }
+}
+
+document.querySelector(".contenedor_boton_top")
+.addEventListener("click", () => {
+  window.scrollTo({
+    top:0,
+    behavior:"smooth"
+  });
+});
+
+// /**------------- FUNCIÓN PARA MOSTRAR EL MODAL ----------------**/
+// const abrir = document.getElementById("modal_id");
+// const cerrar = document.getElementById("close");
+// const modal = document.getElementById("modal");
+
+// abrir.addEventListener("click", function () {
+//     modal.style.display = "block";
+// });
+
+// cerrar.addEventListener("click", function () {
+//     modal.style.display = "none";
+// });
+
+
+
+
+{/* <div class="boton_Modal">
+        <div class="contenedor_modal">
+          <button class="close" id="close">X</button>
+        </div>
+      </div> */}
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,132 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterDir, newPelis, oldPelis } from "../src/data.js";
 
+describe("filterDir", () => {
+  const directores = [
+    {
+      title: "Castle in the Sky",
+      director: "Hayao Miyazaki",
+      release_date: "1986",
+    },
 
-describe('example', () => {
+    {
+      title: "When Marnie Was There",
+      director: "Hiromasa Yonebayashi",
+      release_date: "2014",
+    },
+
+    {
+      title: "Tales from Earthsea",
+      director: "Gorō Miyazaki",
+      release_date: "2006",
+    },
+  ];
+
+  it("returns `Directores que hicieron parte de la pelicula`", () => {
+    expect(filterDir(directores, "Hayao Miyazaki")).toEqual([
+      {
+        title: "Castle in the Sky",
+        director: "Hayao Miyazaki",
+        release_date: "1986",
+      },
+    ]);
+  });
+});
+
+describe('newPelis', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof newPelis).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it("Deberia retornar las peliculas mas nuevas", () => {
+    const pruebaYear = [
+      {
+        title: "Castle in the Sky",
+        director: "Hayao Miyazaki",
+        release_date: "1986",
+      },
+
+      {
+        title: "When Marnie Was There",
+        director: "Hiromasa Yonebayashi",
+        release_date: "2014",
+      },
+
+      {
+        title: "Tales from Earthsea",
+        director: "Gorō Miyazaki",
+        release_date: "2006",
+      },
+    ];
+
+    const resultado = [
+      {
+        title: "When Marnie Was There",
+      director: "Hiromasa Yonebayashi",
+      release_date: "2014",
+      },
+      {
+        title: "Tales from Earthsea",
+        director: "Gorō Miyazaki",
+        release_date: "2006",
+      },
+      {
+        title: "Castle in the Sky",
+        director: "Hayao Miyazaki",
+        release_date: "1986",
+      },
+    ]
+
+    expect(newPelis(pruebaYear)).toEqual(resultado);
   });
 });
 
 
-describe('anotherExample', () => {
+describe('oldPelis', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof oldPelis).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it("Deberia retornar las peliculas mas viejas", () => {
+    const pruebaYear1 = [
+      {
+        title: "Castle in the Sky",
+        director: "Hayao Miyazaki",
+        release_date: "1986",
+      },
+
+      {
+        title: "When Marnie Was There",
+        director: "Hiromasa Yonebayashi",
+        release_date: "2014",
+      },
+
+      {
+        title: "Tales from Earthsea",
+        director: "Gorō Miyazaki",
+        release_date: "2006",
+      },
+    ];
+
+    const resultado1 = [
+      {
+        title: "Castle in the Sky",
+        director: "Hayao Miyazaki",
+        release_date: "1986",
+      },
+
+      {
+        title: "Tales from Earthsea",
+        director: "Gorō Miyazaki",
+        release_date: "2006",
+      },
+
+      {
+        title: "When Marnie Was There",
+      director: "Hiromasa Yonebayashi",
+      release_date: "2014",
+      },
+    ]
+
+    expect(oldPelis(pruebaYear1)).toEqual(resultado1);
   });
 });
